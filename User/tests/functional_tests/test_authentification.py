3aaa8d0a3769f0d8bd887ee66604f03025a11dbb
Jinr0h404 <ejeannenot@msn.com>
2022-01-04 12:18:41 +0000

    change user functional test for adding headless option for chrome

--- User/tests/functional_tests/test_authentification.py
+++ User/tests/functional_tests/test_authentification.py
@@ -5,6 +5,7 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse
 from selenium.webdriver.common.by import By
 from User.models import CustomUser
+from selenium.webdriver.chrome.options import Options
 
 
 class TestAuthentification(StaticLiveServerTestCase):
@@ -12,7 +13,9 @@ class TestAuthentification(StaticLiveServerTestCase):
     def test_signup(self):
         """functional test with selenium to verify the user account creation scenario."""
         self.s = Service("User/tests/functional_tests/chromedriver")
-        self.browser = webdriver.Chrome(service=self.s)
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(service=self.s, options=self.chrome_options)
         self.browser.get(self.live_server_url + reverse("user-signup"))
         username = self.browser.find_element(By.ID, "id_username")
         username.send_keys("ewen_test")
@@ -33,7 +36,9 @@ class TestAuthentification(StaticLiveServerTestCase):
     def test_signin_not_account(self):
         """functional test with selenium to verify the user signin wihtout account."""
         self.s = Service("User/tests/functional_tests/chromedriver")
-        self.browser = webdriver.Chrome(service=self.s)
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(service=self.s, options=self.chrome_options)
         self.browser.get(self.live_server_url + reverse("user-signin"))
         email = self.browser.find_element(By.ID, "id_email")
         email.send_keys("ewen_test@test.com")
@@ -57,7 +62,9 @@ class TestAuthentification(StaticLiveServerTestCase):
         password = "ewen12345"
         CustomUser.objects.create_user(username=username, email=email, password=password)
         self.s = Service("User/tests/functional_tests/chromedriver")
-        self.browser = webdriver.Chrome(service=self.s)
+        self.chrome_options = Options()
+        self.chrome_options.add_argument("--headless")
+        self.browser = webdriver.Chrome(service=self.s, options=self.chrome_options)
         self.browser.get(self.live_server_url + reverse("user-signin"))
         email = self.browser.find_element(By.ID, "id_email")
         email.send_keys("toto@gmail.com")
